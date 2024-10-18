1de97340bea299d12ecb97b000c5c979fb79a013
Alexandre Nascimento da Silva <Alexandrenas2003@gmail.com>
2024-10-18 18:48:40 -0300

    if and else

---
+++ Java_Basic/Day01/entities/Employee.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Employee {
+    private String name;
+    private String role;
+    private Double salary;
+
+    public Employee(String name, String role, Double salary) {
+        this.name = name;
+        this.role = role;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return "Name: " + name + " Role: " + role + " Salary: " + salary;
+    }
+}
